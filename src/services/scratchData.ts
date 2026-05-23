e6d12cd1f6fd60b85835a62bef913f49f9e2c789
Alan <alan4tomaz8@gmail.com>
2026-05-23 17:49:33 -0300

    fix: change the headers for the scratch requisitions

--- src/services/scratchData.ts
+++ src/services/scratchData.ts
@@ -10,7 +10,17 @@ export const scratchDataFromSite = async (url: string) => {
   try {
     const { data } = await axios.get(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+
+        "Accept-Language": "pt-BR,pt;q=0.9,en;q=0.8",
+
+        Referer: "https://www.google.com/",
+
+        Connection: "keep-alive",
       },
     });
 
